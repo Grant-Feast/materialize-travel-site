fc4ad86e2e4acddddf71af118ab88dba1b033558
Grant <feast5801@hotmail.co.uk>
2021-02-22 13:24:05 +0000

    Add weather api
    Use axios to fetch the open weather map api and display the current weather in new york

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Jquery
 $(document).ready(function() {
   $('.sidenav').sidenav();
   $('.materialboxed').materialbox();
@@ -6,3 +7,37 @@ $(document).ready(function() {
   $('.datepicker').datepicker();
   $('.scrollspy').scrollSpy();
 });
+
+// Weather api
+
+const weather = document.querySelector('#weather');
+
+const res = axios.get('http://api.openweathermap.org/data/2.5/weather?q=New York City&units=imperial&appid=5f4c76467089adb3276bc94a084c3e38')
+  .then(res => createElements(res))
+  .catch(err => console.log(err))
+
+  const createElements = (res) => {
+    const weatherIcon = res.data.weather[0].icon;
+    const div = document.createElement('DIV');
+    div.classList = 'row';
+    const h2 = document.createElement('H2');
+    h2.append(`${res.data.name}`);
+    h2.classList = 'indigo-text heading h3';
+    const img = document.createElement('IMG');
+    img.src = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+    const p = document.createElement('P');
+    p.append(`${res.data.weather[0].description}`);
+    div.append(h2, img, p);
+    weather.append(div);
+    const mainData = [`Temperature ${res.data.main.temp}&deg;F`, `Humidity ${res.data.main.humidity}`];
+    mainData.forEach(function(el) {
+      const div = document.createElement('DIV');
+      let span = document.createElement('SPAN');
+      span.innerHTML = el;
+      div.appendChild(span);
+      div.classList = 'col s12 m6';
+      weather.appendChild(div);
+    })
+  }
+
+  
